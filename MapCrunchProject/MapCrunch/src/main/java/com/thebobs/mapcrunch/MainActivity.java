874c8e14d5813772bac0fe91b081e9c302d385be
benjaminhkaiser <benjamin.h.kaiser@gmail.com>
2013-10-21 17:25:45 -0400

    updated layouts to match logo design. font files (.ttf) included in commit.

--- MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/MainActivity.java
+++ MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/MainActivity.java
@@ -1,6 +1,7 @@
 package com.thebobs.mapcrunch;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -14,15 +15,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get typefaces from assets
+        Typeface tfNevis = Typeface.createFromAsset(getAssets(), "fonts/nevis.ttf");
+
+        //Get buttons from view
+        final Button btnStart = (Button) findViewById(R.id.btnStart);
         final Button btnSettings = (Button) findViewById(R.id.btnSettings);
+        final Button btnRankings = (Button) findViewById(R.id.btnRankings);
+
+        btnStart.setTypeface(tfNevis);
+        btnSettings.setTypeface(tfNevis);
+        btnRankings.setTypeface(tfNevis);
+
+        //Set onclick listeners for all buttons
         btnSettings.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 Intent intSettings = new Intent(MainActivity.this, SettingsActivity.class);
                 MainActivity.this.startActivity(intSettings);
             }
         });
-
-        final Button btnRankings = (Button) findViewById(R.id.btnRankings);
         btnRankings.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 Intent intRankings = new Intent(MainActivity.this, RankingsActivity.class);
--- MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/RankingsActivity.java
+++ MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/RankingsActivity.java
@@ -1,15 +1,10 @@
 package com.thebobs.mapcrunch;
 
 import android.app.Activity;
-import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,7 +14,9 @@ public class RankingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rankings);
+
         setTitle("Rankings");
+
         final ListView lstRankings = (ListView) findViewById(R.id.lstRankings);
 
         ArrayList<Ranking> samplerankings = new ArrayList<Ranking>();
@@ -28,10 +25,11 @@ public class RankingsActivity extends Activity {
         samplerankings.add(new Ranking("Sophia", 78.12, 3));
         samplerankings.add(new Ranking("Tim", 12.03, 4));
 
-        ArrayAdapter adapter = new RankingArrayAdapter(this, R.layout.activity_rankings,samplerankings);
+        //Get font from assets to pass into adapter
+        Typeface tfNevis = Typeface.createFromAsset(getAssets(), "fonts/nevis.ttf");
+        ArrayAdapter adapter = new RankingArrayAdapter(this,
+                R.layout.activity_rankings,samplerankings, tfNevis);
 
-        View header = (View)getLayoutInflater().inflate(R.layout.rankings_header_row, null);
-        lstRankings.addHeaderView(header);
         lstRankings.setAdapter(adapter);
         //lstRankings.setOnItemClickListener(new Listener());
     }

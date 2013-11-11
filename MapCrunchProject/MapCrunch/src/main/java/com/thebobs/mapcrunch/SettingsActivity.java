e54314267235b14ba7d9b974e3bce09944332549
benjaminhkaiser <benjamin.h.kaiser@gmail.com>
2013-11-11 16:44:48 -0500

    fixed some problems with the settings menu. clearing the rankings crashes at the moment but i know the button listening is working so it's something with the database code.

--- MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/SettingsActivity.java
+++ MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/SettingsActivity.java
@@ -1,11 +1,14 @@
 package com.thebobs.mapcrunch;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -13,32 +16,43 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.basic_settings);
+
+        final Preference button = (Preference)findPreference("btnClearRankings");
+
+        if (button != null) {
+            button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference arg0) {
+                    //Alex - I know all the code for showing the button and registering the
+                    //click is working. This crashes when you click the button, so I something must
+                    //is wrong with the database stuff.
+                    ScoreDataSource datasource = new ScoreDataSource(SettingsActivity.this);
+                    datasource.open();
+                    datasource.DropTable();
+                    return true;
+                }
+            });
+        }
+
+
     }
 
-    /**
-     * Populate the activity with the top-level headers.
-     */
+    /*
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.preference_headers, target);
     }
 
-    /**
-     * This fragment shows the preferences for the first header (basic settings)
-     */
     public static class BasicSettingsFragment extends PreferenceFragment {
-        Context context = getActivity().getApplicationContext();
+        //Context context = getActivity().getApplicationContext();
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
-            //TODO: May need to set default value at some point - this is how it's done
-            //PreferenceManager.setDefaultValues(getActivity(),
-            //        R.xml.advanced_preferences, false);
-
             addPreferencesFromResource(R.xml.basic_settings);
 
-            Preference button = (Preference)findPreference("button");
+            /*Preference button = (Preference)findPreference("button");
             button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
 
@@ -50,22 +64,5 @@ public class SettingsActivity extends PreferenceActivity {
                 }
             });
         }
-    }
-
-    /**
-     * This fragment shows the preferences for the second header (advanced settings)
-     */
-    public static class AdvSettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            //TODO: May need to set default value at some point - this is how it's done
-            //PreferenceManager.setDefaultValues(getActivity(),
-            //        R.xml.advanced_settings, false);
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.advanced_settings);
-        }
-    }
+    }*/
 }

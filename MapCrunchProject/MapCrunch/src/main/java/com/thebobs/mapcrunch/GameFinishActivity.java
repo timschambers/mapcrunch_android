ded19d53e0d1fc5964b2bf5e4e3f7e606de9829e
benjaminhkaiser <benjamin.h.kaiser@gmail.com>
2013-11-20 17:28:28 -0500

    last import cleanup before merge

--- MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/GameFinishActivity.java
+++ MapCrunchProject/MapCrunch/src/main/java/com/thebobs/mapcrunch/GameFinishActivity.java
@@ -2,16 +2,12 @@ package com.thebobs.mapcrunch;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Random;
 
 /**
  * Created by ben on 11/9/13.
